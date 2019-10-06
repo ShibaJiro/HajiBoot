02f4dade6a6a101b1b63200c7cde520fad504f8e
ShibaJiro <shiba.jiro.web@gmail.com>
2019-10-06 22:54:46 +0900

    2019/10/06 [3.3] Thymeleafを使った、画面のあるWebアプリの開発 終了

---
+++ HajiBootThymeleaf/HajibootThymeleaf/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Snippet {
+    public static void main(String[] args) {
+        <dependency>
+            <groupId>org.lazyluke</groupId>
+            <artifactId>log4jdbc-remix</artifactId>
+            <version>0.2.7</version>
+        </dependency>
+    }
+}
+
